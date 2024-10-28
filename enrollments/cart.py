6c61b3d8f1869d1bbe2e451b6281332cd4724299
itsMajid <itsMajid.dev@gmail.com>
2024-10-28 22:26:19 +0330

    create remove mothod for delete product for cart

--- enrollments/cart.py
+++ enrollments/cart.py
@@ -20,3 +20,10 @@ class cart:
     def save(self):
         self.session.modified = True
 
+    def remove(self , product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.save()
+
+
