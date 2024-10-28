197a180f80d7dc34591b157275c75edf2a7f4abf
jalil <95492487+leonardo0231@users.noreply.github.com>
2024-10-28 13:06:16 +0330

    Create base of mdoel

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class LessonModel(models.Model):
+    title = models.CharField(
+        max_length=255,
+    )
+    content = models.TextField()
+    course_id = models.OneToOneField()
+    week_number = models.IntegerField()
+    order = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+     
